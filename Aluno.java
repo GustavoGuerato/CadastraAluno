1800ec42af201ca1dae0d36a6cd80092eb4ed0ef
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-03-04 16:38:53 -0300

    creation of media method

--- Aluno.java
+++ Aluno.java
@@ -2,7 +2,7 @@ package JdevCurso.cadastroAluno;
 
 public class Aluno {
 
-   /*esses são os atributos da classe aluno */
+   /* esses são os atributos da classe aluno */
    private String nome;
    private int idade;
    private String nascimento;
@@ -13,68 +13,127 @@ public class Aluno {
    private String matricula;
    private String escola;
    private String serieMatriculado;
-   public Aluno(){/*cria os dados na memoria java */
+   private double nota1;
+   private double nota2;
+   private double nota3;
+   private double nota4;
 
-   }
+   public Aluno() {
+      /* cria os dados na memoria java */}
+
    public String getNome() {
       return nome;
    }
+
    public void setNome(String nome) {
       this.nome = nome;
    }
+
    public int getIdade() {
       return idade;
    }
+
    public void setIdade(int idade) {
       this.idade = idade;
    }
+
    public String getNascimento() {
       return nascimento;
    }
+
    public void setNascimento(String nascimento) {
       this.nascimento = nascimento;
    }
+
    public String getRg() {
       return rg;
    }
+
    public void setRg(String rg) {
       this.rg = rg;
    }
+
    public int getNumeroCPF() {
       return numeroCPF;
    }
+
    public void setNumeroCPF(int numeroCPF) {
       this.numeroCPF = numeroCPF;
    }
+
    public String getNomeMae() {
       return nomeMae;
    }
+
    public void setNomeMae(String nomeMae) {
       this.nomeMae = nomeMae;
    }
+
    public String getNomePai() {
       return nomePai;
    }
+
    public void setNomePai(String nomePai) {
       this.nomePai = nomePai;
    }
+
    public String getMatricula() {
       return matricula;
    }
+
    public void setMatricula(String matricula) {
       this.matricula = matricula;
    }
+
    public String getEscola() {
       return escola;
    }
+
    public void setEscola(String escola) {
       this.escola = escola;
    }
+
    public String getSerieMatriculado() {
       return serieMatriculado;
    }
+
    public void setSerieMatriculado(String serieMatriculado) {
       this.serieMatriculado = serieMatriculado;
    }
-}
 
+   public double getNota1() {
+      return nota1;
+   }
+
+   public void setNota1(double nota1) {
+      this.nota1 = nota1;
+   }
+
+   public double getNota2() {
+      return nota2;
+   }
+
+   public void setNota2(double nota2) {
+      this.nota2 = nota2;
+   }
+
+   public double getNota3() {
+      return nota3;
+   }
+
+   public void setNota3(double nota3) {
+      this.nota3 = nota3;
+   }
+
+   public double getNota4() {
+      return nota4;
+   }
+
+   public void setNota4(double nota4) {
+      this.nota4 = nota4;
+   }
+
+   public double getMediaNota(){
+      return(nota1 + nota2 + nota3 + nota4)/4;
+   }
+}
--- cadastraAluno.java
+++ cadastraAluno.java
@@ -11,9 +11,12 @@ public class cadastraAluno {
     aluno1.setNascimento("14/08/2004");
     aluno1.setRg("49.977.657-4");
     aluno1.setNumeroCPF(321340677);
+    aluno1.setNota1(95);
+    aluno1.setNota2(87.65);
+    aluno1.setNota3(69);
+    aluno1.setNota4(78.07);
 
-    System.out.println(aluno1.getNome());
-    Aluno aluno2 = new Aluno();
-    Aluno aluno3 = new Aluno();
+    System.out.println(aluno1.getMediaNota());
+   
   }
 }
