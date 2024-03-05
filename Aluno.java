d3f9b5fd2ecc2a56be11679939f7905bb5571171
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-03-05 19:43:10 -0300

    creation of disclipina method

--- Aluno.java
+++ Aluno.java
@@ -13,10 +13,16 @@ public class Aluno {
    private String matricula;
    private String escola;
    private String serieMatriculado;
-   private double nota1;
-   private double nota2;
-   private double nota3;
-   private double nota4;
+
+   private Disciplina disciplina = new Disciplina();
+
+   public void setDisciplina(Disciplina disciplina) {
+      this.disciplina = disciplina;
+   }
+
+   public Disciplina getDisciplina() {
+      return disciplina;
+   }
 
    public Aluno() {
       /* cria os dados na memoria java */}
@@ -101,40 +107,8 @@ public class Aluno {
       this.serieMatriculado = serieMatriculado;
    }
 
-   public double getNota1() {
-      return nota1;
-   }
-
-   public void setNota1(double nota1) {
-      this.nota1 = nota1;
-   }
-
-   public double getNota2() {
-      return nota2;
-   }
-
-   public void setNota2(double nota2) {
-      this.nota2 = nota2;
-   }
-
-   public double getNota3() {
-      return nota3;
-   }
-
-   public void setNota3(double nota3) {
-      this.nota3 = nota3;
-   }
-
-   public double getNota4() {
-      return nota4;
-   }
-
-   public void setNota4(double nota4) {
-      this.nota4 = nota4;
-   }
-
    public double getMediaNota() {
-      return (nota1 + nota2 + nota3 + nota4) / 4;
+      return (disciplina.getNota1() + disciplina.getNota2() + disciplina.getNota3() + disciplina.getNota4()) / 4;
    }
 
    public boolean getAlunoAprovado() {
@@ -146,12 +120,13 @@ public class Aluno {
       }
    }
 
+   
+
    @Override
    public String toString() {
       return "Aluno [nome=" + nome + ", idade=" + idade + ", nascimento=" + nascimento + ", rg=" + rg + ", numeroCPF="
             + numeroCPF + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai + ", matricula=" + matricula + ", escola="
-            + escola + ", serieMatriculado=" + serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3="
-            + nota3 + ", nota4=" + nota4 + "]";
+            + escola + ", serieMatriculado=" + serieMatriculado + ", disciplina=" + disciplina + "]";
    }
 
    @Override
@@ -188,6 +163,4 @@ public class Aluno {
       return true;
    }
 
-
-   
 }
--- cadastraAluno.java
+++ cadastraAluno.java
@@ -8,89 +8,88 @@ public class cadastraAluno {
     /* new aluno é uma instancia (criação de objeto) */
     /* aluno1 é uma referencia para o objeto aluno */
 
-   
-      String nome = JOptionPane.showInputDialog("qual o nome do aluno: ");
-      
-      String inputIdade = JOptionPane.showInputDialog("digite a idade do aluno: ");
-      int idadeAluno = Integer.parseInt(inputIdade);
-      
-      String nascimento =
-      JOptionPane.showInputDialog("qual a data de nascimento do aluno: ");
-      
-      String rg = JOptionPane.showInputDialog("qual o rg do aluno: ");
-      
-      String inputCpf = JOptionPane.showInputDialog("qual o cpf do aluno: ");
-      int cpfAluno = Integer.parseInt(inputCpf);
-      
-      String nomeMae =
-      JOptionPane.showInputDialog("qual o nome da mae do aluno: ");
-      
-      String nomePai =
-      JOptionPane.showInputDialog("qual o nome do pai do aluno: ");
-      
-      String matricula =
-      JOptionPane.showInputDialog("qual a data de matricula do aluno: ");
-      
-      String escola = JOptionPane.
-      showInputDialog("qual o nome da escola que o aluno está se matriculando: ");
-      
-      String serieMatriculado =
-      JOptionPane.showInputDialog("qual a serie do aluno: ");
-      
-      String inputNota1 =
-      JOptionPane.showInputDialog("insira o valor da nota 1: ");
-      double nota1 = Double.parseDouble(inputNota1);
-      
-      String inputNota2 =
-      JOptionPane.showInputDialog("insira o valor da nota 2: ");
-      double nota2 = Double.parseDouble(inputNota2);
-      
-      String inputNota3 =
-      JOptionPane.showInputDialog("insira o valor da nota 3: ");
-      double nota3 = Double.parseDouble(inputNota3);
-      
-      String inputNota4 =
-      JOptionPane.showInputDialog("insira o valor da nota 4: ");
-      double nota4 = Double.parseDouble(inputNota4);
-      
-      Aluno aluno1 = new Aluno();
-      
-      aluno1.setNome(nome);
-      aluno1.setIdade(idadeAluno);
-      aluno1.setNascimento(nascimento);
-      aluno1.setRg(rg);
-      aluno1.setNumeroCPF(cpfAluno);
-      aluno1.setNomeMae(nomeMae);
-      aluno1.setNomePai(nomePai);
-      aluno1.setMatricula(matricula);
-      aluno1.setEscola(escola);
-      aluno1.setSerieMatriculado(serieMatriculado);
-      aluno1.setNota1(nota1);
-      aluno1.setNota2(nota2);
-      aluno1.setNota3(nota3);
-      aluno1.setNota4(nota4);
+    String nome = JOptionPane.showInputDialog("qual o nome do aluno: ");
 
-      
-      System.out.println(aluno1.toString());
-     
-      Aluno aluno2 = new Aluno();
+    String inputIdade = JOptionPane.showInputDialog("digite a idade do aluno: ");
+    int idadeAluno = Integer.parseInt(inputIdade);
 
-      aluno2.setNome(nome);
-      aluno2.setIdade(idadeAluno);
-      aluno2.setNascimento(nascimento);
-      aluno2.setRg(rg);
-      aluno2.setNumeroCPF(cpfAluno);
-      aluno2.setNomeMae(nomeMae);
-      aluno2.setNomePai(nomePai);
-      aluno2.setMatricula(matricula);
-      aluno2.setEscola(escola);
-      aluno2.setSerieMatriculado(serieMatriculado);
-      aluno2.setNota1(nota1);
-      aluno2.setNota2(nota2);
-      aluno2.setNota3(nota3);
-      aluno2.setNota4(nota4);
-    
-  
- 
+    String nascimento = JOptionPane.showInputDialog("qual a data de nascimento do aluno: ");
+
+    String rg = JOptionPane.showInputDialog("qual o rg do aluno: ");
+
+    String inputCpf = JOptionPane.showInputDialog("qual o cpf do aluno: ");
+    int cpfAluno = Integer.parseInt(inputCpf);
+
+    String nomeMae = JOptionPane.showInputDialog("qual o nome da mae do aluno: ");
+
+    String nomePai = JOptionPane.showInputDialog("qual o nome do pai do aluno: ");
+
+    String matricula = JOptionPane.showInputDialog("qual a data de matricula do aluno: ");
+
+    String escola = JOptionPane.showInputDialog("qual o nome da escola que o aluno está se matriculando: ");
+
+    String serieMatriculado = JOptionPane.showInputDialog("qual a serie do aluno: ");
+
+    String disciplina1 = JOptionPane.showInputDialog("qual o nome da primeira materia? ");
+    String disciplina2 = JOptionPane.showInputDialog("qual o nome da segunda materia? ");
+    String disciplina3 = JOptionPane.showInputDialog("qual o nome da terceira materia? ");
+    String disciplina4 = JOptionPane.showInputDialog("qual o nome da quarta materia? ");
+
+    String inputNota1 = JOptionPane.showInputDialog("insira o valor da nota 1: ");
+    double nota1 = Double.parseDouble(inputNota1);
+
+    String inputNota2 = JOptionPane.showInputDialog("insira o valor da nota 2: ");
+    double nota2 = Double.parseDouble(inputNota2);
+
+    String inputNota3 = JOptionPane.showInputDialog("insira o valor da nota 3: ");
+    double nota3 = Double.parseDouble(inputNota3);
+
+    String inputNota4 = JOptionPane.showInputDialog("insira o valor da nota 4: ");
+    double nota4 = Double.parseDouble(inputNota4);
+
+    Aluno aluno1 = new Aluno();
+
+    aluno1.setNome(nome);
+    aluno1.setIdade(idadeAluno);
+    aluno1.setNascimento(nascimento);
+    aluno1.setRg(rg);
+    aluno1.setNumeroCPF(cpfAluno);
+    aluno1.setNomeMae(nomeMae);
+    aluno1.setNomePai(nomePai);
+    aluno1.setMatricula(matricula);
+    aluno1.setEscola(escola);
+    aluno1.setSerieMatriculado(serieMatriculado);
+    aluno1.getDisciplina().setDisciplina1(disciplina1);
+    aluno1.getDisciplina().setDisciplina2(disciplina2);
+    aluno1.getDisciplina().setDisciplina3(disciplina3);
+    aluno1.getDisciplina().setDisciplina4(disciplina4);
+    aluno1.getDisciplina().setNota1(nota1);
+    aluno1.getDisciplina().setNota2(nota2);
+    aluno1.getDisciplina().setNota3(nota3);
+    aluno1.getDisciplina().setNota4(nota4);
+
+    System.out.println(aluno1.toString());
+
+    Aluno aluno2 = new Aluno();
+
+    aluno2.setNome(nome);
+    aluno2.setIdade(idadeAluno);
+    aluno2.setNascimento(nascimento);
+    aluno2.setRg(rg);
+    aluno2.setNumeroCPF(cpfAluno);
+    aluno2.setNomeMae(nomeMae);
+    aluno2.setNomePai(nomePai);
+    aluno2.setMatricula(matricula);
+    aluno2.setEscola(escola);
+    aluno2.setSerieMatriculado(serieMatriculado);
+    aluno2.getDisciplina().setDisciplina1(disciplina1);
+    aluno2.getDisciplina().setDisciplina2(disciplina2);
+    aluno2.getDisciplina().setDisciplina3(disciplina3);
+    aluno2.getDisciplina().setDisciplina4(disciplina4);
+    aluno2.getDisciplina().setNota1(nota1);
+    aluno2.getDisciplina().setNota2(nota2);
+    aluno2.getDisciplina().setNota3(nota3);
+    aluno2.getDisciplina().setNota4(nota4);
+
   }
 }
